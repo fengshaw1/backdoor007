b65de00a8ca7eb02d1d559f13d745bdf0ceb357b
Frank Shaw <xiaofrank77@gmail.com>
2024-11-06 15:50:49 +0800

    add stepping

--- utils/image_helper.py
+++ utils/image_helper.py
@@ -12,20 +12,27 @@ class ImageHelper(Helper):   # 定义一个名为 ImageHelper 的类，继承自
     test_loader = None   # 类变量，初始化为 None，用于存储测试数据加载器
 
     def load_cifar10(self, batch_size):   # 定义一个方法 load_cifar10，接受一个参数 batch_size，表示批量大小
-        transform = transforms.Compose(   # 使用 Compose 方法将多个转换操作组合在一起
-            [transforms.ToTensor(),   # 将图像转换为 PyTorch 张量，像素值从 0-255 范围缩放到 0-1
-             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])   # 对图像进行归一化，(0.5, 0.5, 0.5) 是均值，(0.5, 0.5, 0.5) 是标准差
+        transform_train = transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ])
+        transform_test = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ])
 
         # 加载 CIFAR-10 数据集的训练集，transform 参数用于数据预处理
         self.train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                                download=True, transform=transform)
+                                                download=True, transform=transform_train)
         # 使用 DataLoader 加载训练集，batch_size 设置为传入的批量大小，shuffle=True 表示随机打乱数据
         self.train_loader = torch.utils.data.DataLoader(self.train_dataset, batch_size=batch_size,
                                                   shuffle=True, num_workers=2)  # num_workers=2 表示加载数据时使用两个子进程
 
         # 加载 CIFAR-10 数据集的测试集，transform 参数用于数据预处理
         self.test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                               download=True, transform=transform)
+                                               download=True, transform=transform_test)
         # 使用 DataLoader 加载测试集，batch_size 设置为 10，shuffle=False 表示不打乱数据
         self.test_loader = torch.utils.data.DataLoader(self.test_dataset, batch_size=10,
                                                  shuffle=False, num_workers=2)
